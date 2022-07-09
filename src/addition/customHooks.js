ecec98ebf2279c1854e9d46d0cd4bd77c376ae1d
ksfgn <shapovalovalex938@gmail.com>
2022-07-10 00:47:59 +0300

    log deleted

--- src/addition/customHooks.js
+++ src/addition/customHooks.js
@@ -30,7 +30,6 @@ export const useInput = (initialValue, validations) => {
   const [value, setValue] = useState(initialValue);
   const [isDirty, setDirty] = useState(false);
   const valid = useValidation(value, validations);
-
   const onChange = (e) => setValue(e.target.value);
   const onBlur = (e) => setDirty(true);
   const clear = () => setValue("");
